fabfa250422875b5bae4e80900e1fd1757cec3d1
kalyani7393 <kynir.73@gmail.com>
2026-02-25 10:03:32 -0800

    retry

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -28,7 +28,7 @@ const FeatureSection = ({
           <img
             src={imageSrc}
             alt={imageAlt}
-            className="w-full h-[400px] md:h-[350px] lg:h-[550px] object-contain object-top bg-black/5 rounded-xl"
+            className="w-full h-auto rounded-xl"
             loading="lazy"
           />
         </div>
