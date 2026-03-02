94e79e385f7e526b83460dc2ae52c8237780b2a0
kalyani7393 <kynir.73@gmail.com>
2026-03-01 16:26:42 -0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+if (redirect) {
+  window.history.replaceState(null, "", decodeURIComponent(redirect));
+}
 createRoot(document.getElementById("root")!).render(<App />);
